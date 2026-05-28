60a1d2373b65f7ccac9d879b2d75a02106aee4fb
Sara Aquino <aquinosara2019@gmail.com>
2026-05-28 10:33:22 +0200

    Modifica form

--- Gestione_Piattaforma_Materiale_Didattico/src/main/java/boundary/LoginForm.java
+++ Gestione_Piattaforma_Materiale_Didattico/src/main/java/boundary/LoginForm.java
@@ -4,10 +4,10 @@ import javax.swing.*;
 
 public class LoginForm {
     private JPanel contentPane;
-    private JTextField txtBenvenuto;
-    private JPasswordField txtPassword;
     private JTextField txtEmail;
-    private JLabel Email;
-    private JLabel Password;
-    private JButton btnIscrizione;
+    private JPasswordField txtPassword;
+    private JButton confermaButton;
+    private JLabel imgLogo;
 }
+
+
