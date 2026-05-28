65ed3f2b3bb2fdda1e33f8d65ea2e7205c663802
Sara Aquino <aquinosara2019@gmail.com>
2026-05-28 09:47:17 +0200

    Form

---
+++ Gestione_Piattaforma_Materiale_Didattico/src/main/java/boundary/LoginForm.java
@@ -0,0 +1,13 @@
+package boundary;
+
+import javax.swing.*;
+
+public class LoginForm {
+    private JPanel contentPane;
+    private JTextField txtBenvenuto;
+    private JPasswordField txtPassword;
+    private JTextField txtEmail;
+    private JLabel Email;
+    private JLabel Password;
+    private JButton btnIscrizione;
+}
